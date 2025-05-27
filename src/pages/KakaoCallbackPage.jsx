5197e9ddebd6102e42b24dc1ebd435af12107b60
KimJunSeo <rlawnstj901+2@gmail.com>
2025-05-27 17:30:54 +0900

    Fix: 카카오 로그인 수정

--- src/pages/KakaoCallbackPage.jsx
+++ src/pages/KakaoCallbackPage.jsx
@@ -1,38 +1,17 @@
 import { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-import axios from 'axios'
-import { useDispatch } from 'react-redux'
-import { setUserInfo } from '../store/userSlice'
 
 const API_URL = import.meta.env.VITE_BACK_URL
 
 export const KakaoCallbackPage = () => {
-  const navigate = useNavigate()
-  const dispatch = useDispatch()
-
   useEffect(() => {
-    const handleKakaoLogin = async () => {
-      const code = new URLSearchParams(window.location.search).get('code')
-      if (!code) {
-        alert('로그인 코드가 없습니다.')
-        return
-      }
-
-      try {
-        const res = await axios.get(`${API_URL}/auth/kakao/callback?code=${code}`, {
-          withCredentials: true,
-        })
-        dispatch(setUserInfo(res.data))
-        navigate('/')
-      } catch (err) {
-        console.error('카카오 로그인 실패:', err)
-        alert('카카오 로그인 실패')
-        navigate('/login')
-      }
+    const code = new URLSearchParams(window.location.search).get('code')
+    if (!code) {
+      alert('로그인 코드가 없습니다.')
+      return
     }
 
-    handleKakaoLogin()
-  }, [navigate, dispatch])
+    window.location.href = `${API_URL}/auth/kakao/callback?code=${code}`
+  }, [])
 
   return <div>카카오 로그인 처리 중입니다...</div>
 }
